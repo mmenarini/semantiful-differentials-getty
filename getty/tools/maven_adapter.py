3f47c32e7a0a9f02fb9fad6c12599579d7855401
Sander Valstar <sander@email.com>
2018-06-14 00:45:52 -0700

    optimize adapter using json file

--- getty/tools/maven_adapter.py
+++ getty/tools/maven_adapter.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import
+
+import json
 from os import path
 
 from . import mvn, os, git_adapter
 from .project_utils import ProjectUtils
+
 
 def create_version(hash):
     git_adapter.checkout(hash)
@@ -10,12 +13,13 @@ def create_version(hash):
     mvn.clean(ProjectUtils.project_path)
 
     copy_cmd = " ".join(["cp", "-R",
-                          ProjectUtils.project_path + "/",
-                          ProjectUtils.get_version_path(hash)
-                          ])
+                         ProjectUtils.project_path + "/",
+                         ProjectUtils.get_version_path(hash)
+                         ])
 
     print "copy command =", copy_cmd
     os.sys_call(copy_cmd)
+    save_paths(hash, {})
 
 
 def create_version_if_not_exists(hash):
@@ -26,43 +30,77 @@ def create_version_if_not_exists(hash):
         create_version(hash)
 
 
-def get_bin_path(hash):
+def save_paths(hash, paths):
+    fp = ProjectUtils.get_version_path(hash) + "/paths.json"
+    with open(fp, 'w+') as outfile:
+        json.dump(paths, outfile)
+
+
+def get_paths(hash):
+    fp = ProjectUtils.get_version_path(hash) + "/paths.json"
+    with open(fp) as infile:
+        return json.load(infile)
+
+
+def get_path(hash, name):
     create_version_if_not_exists(hash)
-    # todo: return from json if exists
-    return mvn.path_from_mvn_call("outputDirectory", cwd=ProjectUtils.get_version_path(hash))
+
+    paths = get_paths(hash)
+    if name not in paths:
+        paths[name] = mvn.path_from_mvn_call(name, cwd=ProjectUtils.get_version_path(hash))
+        save_paths(hash, paths)
+
+    return paths[name]
+
+
+def get_bin_path(hash):
+    return get_path(hash, "outputDirectory")
 
 
 def get_test_bin_path(hash):
-    create_version_if_not_exists(hash)
-    return mvn.path_from_mvn_call("testOutputDirectory", cwd=ProjectUtils.get_version_path(hash))
+    return get_path(hash, "testOutputDirectory")
 
 
 def get_source_directory(hash):
-    create_version_if_not_exists(hash)
-    return mvn.path_from_mvn_call("sourceDirectory", cwd=ProjectUtils.get_version_path(hash))
+    return get_path(hash, "sourceDirectory")
 
 
 def get_test_source_directory(hash):
-    create_version_if_not_exists(hash)
-    return mvn.path_from_mvn_call("testSourceDirectory", cwd=ProjectUtils.get_version_path(hash))
+    return get_path(hash, "testSourceDirectory")
 
 
 # TODO: this probably will still use the paths to the original project dir
 def get_full_class_path(hash, junit_path, sys_classpath, bin_output, test_output):
-    create_version_if_not_exists(hash)
-    return mvn.full_classpath(junit_path, sys_classpath, bin_output, test_output)
+    paths = get_paths(hash)
+    name = "fullClassPath"
+    if name not in paths:
+        paths[name] = mvn.full_classpath(junit_path, sys_classpath, bin_output, test_output)
+        save_paths(hash, paths)
 
-
-def compile_tests(hash):
-    create_version_if_not_exists(hash)
-    mvn.test_compile(ProjectUtils.get_version_path(hash))
+    return paths[name]
 
 
 def get_junit_torun(cust_mvn_repo, hash):
-    create_version_if_not_exists(hash)
-    return mvn.junit_torun_str(cust_mvn_repo)
+    paths = get_paths(hash)
+    name = "junitToRun"
+    if name not in paths:
+        paths[name] = mvn.junit_torun_str(cust_mvn_repo)
+        save_paths(hash, paths)
+
+    return paths[name]
+
+
+def do_command_if_not_done_yet(hash, command, *args):
+    paths = get_paths(hash)
+    name = command.func_name
+    if name not in paths:
+        command(*args)
+        paths[name] = "done"
+        save_paths(hash, paths)
+
+def compile_tests(hash):
+    do_command_if_not_done_yet(hash, mvn.test_compile, ProjectUtils.get_version_path(hash))
 
 
 def generate_test_report(go, hash):
-    create_version_if_not_exists(hash)
-    mvn.generate_coverage_report(go, hash)
+    do_command_if_not_done_yet(hash, mvn.generate_coverage_report, go, hash)
